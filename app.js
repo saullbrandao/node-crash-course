c632b81737278461318e3206825b95d62543494c
Saull Brandão <saull@outlook.com>
2021-07-21 09:21:28 -0300

    middlewares

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 // register view engine
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   const blogs = [
@@ -11,29 +15,20 @@ app.get('/', (req, res) => {
     { title: 'Title 2', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
     { title: 'Title 3', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
   ]
-  res.render('index', {
-    title: 'Home',
-    blogs,
-  })
+  res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About',
-  })
+  res.render('about', { title: 'About' })
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create', {
-    title: 'Create a new Blog',
-  })
+  res.render('create', { title: 'Create a new Blog' })
 })
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', {
-    title: '404',
-  })
+  res.status(404).render('404', { title: '404' })
 })
 
 app.listen('8080', () => {
