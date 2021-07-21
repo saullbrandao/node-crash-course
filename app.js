5eda8b651bad918c5b013a19f3c9a9dd2e82944e
Saull Brandão <saull@outlook.com>
2021-07-21 08:16:20 -0300

    express 101

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', {
+    root: __dirname,
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', {
+    root: __dirname,
+  })
+})
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about')
+})
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', {
+    root: __dirname,
+  })
+})
+
+app.listen('8080', () => {
+  console.log('🚀 listening for requests on port 8080')
+})
