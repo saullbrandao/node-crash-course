246815b48309be8f1dba375c08e26665a5f4d64f
Saull Brandão <saull@outlook.com>
2021-07-21 10:39:45 -0300

    mongodb and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+require('dotenv').config()
 
 const app = express()
+
+// connect to mongodb
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgvpm.mongodb.net/node-crash-course?retryWrites=true&w=majority`
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    app.listen('8080', () => {
+      console.log('🚀 listening for requests on port 8080')
+    })
+  })
+  .catch(err => console.log(err))
 
 // register view engine
 app.set('view engine', 'ejs')
@@ -9,17 +23,26 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(morgan('dev'))
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    { title: 'Title 1', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
-    { title: 'Title 2', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
-    { title: 'Title 3', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
-  ]
-  res.render('index', { title: 'Home', blogs })
+  res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' })
+})
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', {
+        title: 'All Blogs',
+        blogs: result,
+      })
+    })
+    .catch(err => console.log(err))
 })
 
 app.get('/blogs/create', (req, res) => {
@@ -29,8 +52,4 @@ app.get('/blogs/create', (req, res) => {
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' })
-})
-
-app.listen('8080', () => {
-  console.log('🚀 listening for requests on port 8080')
 })
