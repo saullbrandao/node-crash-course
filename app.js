366e0aee8b3ab3eaea6f00d1e1033e306a5ac271
Saull Brandão <saull@outlook.com>
2021-07-21 09:03:43 -0300

    view engines

--- app.js
+++ app.js
@@ -2,27 +2,37 @@ const express = require('express')
 
 const app = express()
 
+// register view engine
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {
-    root: __dirname,
+  const blogs = [
+    { title: 'Title 1', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+    { title: 'Title 2', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+    { title: 'Title 3', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
+  ]
+  res.render('index', {
+    title: 'Home',
+    blogs,
   })
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', {
-    root: __dirname,
+  res.render('about', {
+    title: 'About',
   })
 })
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {
+    title: 'Create a new Blog',
+  })
 })
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', {
-    root: __dirname,
+  res.status(404).render('404', {
+    title: '404',
   })
 })
 
